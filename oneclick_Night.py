f9dfd14e0cb02af5aeeb18a59ed797501cc92f33
Thomas <thomas.leong@benchmarktechmalaysia.com>
2024-06-10 18:00:56 +0800

    Remove unusable Screenshot Photo

--- oneclick_Night.py
+++
@@ -1,8 +0,0 @@
-#exec(open('zentao_noctool_kaidan.py').read())
-exec(open('aliyun.py').read())
-exec(open('tencent.py').read())
-exec(open('huawei.py').read())
-exec(open('ucloud.py').read())
-exec(open('gname_juming_7211_com.py').read())
-exec(open('zentaowater.py').read())
-exec(open('noctool_water.py').read())
