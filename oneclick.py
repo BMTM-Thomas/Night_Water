462f0a013e2338201dfa486492867d756c377aa0
BMTM-Thomas <168662185+BMTM-Thomas@users.noreply.github.com>
2024-07-04 14:00:52 +0800

    adding git pull origin main

--- oneclick.py
+++ oneclick.py
@@ -1,3 +1,21 @@
+import subprocess
+
+# Example command to list files in the current directory
+command = "git push origin main"
+
+# Execute the command
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Wait for the command to complete and capture the output
+stdout, stderr = process.communicate()
+
+# Decode the byte strings to UTF-8 and print the output
+if stdout:
+    print(stdout.decode('utf-8'))
+if stderr:
+    print(stderr.decode('utf-8'))
+
+
 # exec(open('zentao_noctool_kaidan.py').read())
 exec(open('aliyun.py').read())
 exec(open('tencent.py').read())
@@ -6,4 +24,3 @@ exec(open('ucloud.py').read())
 exec(open('gname_juming_7211_com.py').read())
 exec(open('zentaowater.py').read())
 exec(open('noctoolwater.py').read())
-
