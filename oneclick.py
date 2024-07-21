5c0c5593e2a53035ac4d7268ab183d471452f1d6
BMTM-Thomas <168662185+BMTM-Thomas@users.noreply.github.com>
2024-07-21 09:03:23 +0800

    testing

--- oneclick.py
+++ oneclick.py
@@ -17,7 +17,7 @@ if stderr:
 
 
 exec(open('Run_Check.py').read())
-# exec(open('zentao_noctool_kaidan.py').read())
+exec(open('zentao_noctool_kaidan.py').read())
 exec(open('aliyun.py').read())
 exec(open('tencent.py').read())
 exec(open('huawei.py').read())
@@ -25,4 +25,4 @@ exec(open('ucloud.py').read())
 exec(open('gname_juming_7211_com.py').read())
 exec(open('zentaowater.py').read())
 exec(open('noctoolwater.py').read())
-# exec(open('zentao_noctool_guandan.py').read())
+exec(open('zentao_noctool_guandan.py').read())
