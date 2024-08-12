0bbbb264a3154d2d3cfb66651f4049e1ac035a60
BMTM-Thomas <168662185+BMTM-Thomas@users.noreply.github.com>
2024-08-13 00:28:14 +0800

    update mongodb cmd

--- mongodb_cmd.py
+++ mongodb_cmd.py
@@ -24,6 +24,9 @@ db.Night_Database.findOne(
    { "Ven_Machine": { "$regex": /ven29/i } })
 
 
+# Delete one 
+db.Night_Database.deleteOne({Ven_Machine: {"$regex": /ven239/i}})
+
 #Find and Modify
 db.Night_Database.findAndModify({
 query: {"Ven_Machine": { "$regex": /ven290/i}},
