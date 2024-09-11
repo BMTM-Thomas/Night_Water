64ea1834fbb27c16bd8039517deb7af9d9887e65
Thomas <samleong6812@hotmail.com>
2024-09-11 14:18:47 +0800

    add pull github repos

--- oneclick.py
+++ oneclick.py
@@ -1,30 +1,19 @@
 import subprocess
 
-# # Example command to list files in the current directory
-# command = "git pull origin main"
+# pull repos command
+command = "git pull origin main"
 
-# # Execute the command
-# process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# Execute the command
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# # Wait for the command to complete and capture the output
-# stdout, stderr = process.communicate()
+# Wait for the command to complete and capture the output
+stdout, stderr = process.communicate()
 
-# # Decode the byte strings to UTF-8 and print the output
-# if stdout:
-#     print(stdout.decode('utf-8'))
-# if stderr:
-#     print(stderr.decode('utf-8'))
-
-# exec(open('./Run_Check.py').read())
-# exec(open('./zentao_noctool_kaidan.py').read())
-# exec(open('./aliyun.py').read())
-# exec(open('./tencent.py').read())
-# exec(open('./huawei.py').read())
-# exec(open('./ucloud.py').read())
-# exec(open('./gname_juming_7211_com.py').read())
-# exec(open('./zentaowater.py').read())
-# exec(open('./noctoolwater.py').read())
-# exec(open('./zentao_noctool_zanting.py').read()) 
+# Decode the byte strings to UTF-8 and print the output
+if stdout:
+    print(stdout.decode('utf-8'))
+if stderr:
+    print(stderr.decode('utf-8'))
 
 subprocess.run(["python3", "./Run_Check.py"])
 subprocess.run(["python3", "./zentao_noctool_kaidan.py"])
