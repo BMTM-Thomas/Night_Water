b436e08b11fc428a2587279ea1be2872d1b5ffd5
Thomas <samleong6812@hotmail.com>
2024-09-18 13:57:48 +0800

    bugfix cannot write text, change to use copy and paste text

--- noctoolwater.py
+++ noctoolwater.py
@@ -1,9 +1,12 @@
 import time
 import pyautogui
+import pyperclip
+from pynput.keyboard import Controller
 from List_Zentao import ID, mongodb_id
 from List_Noctool import n_webpage
 from bson.objectid import ObjectId  
 from function import chrome, wait, find_one, find_element_nontext, mongodb_atlas
+
 
 # noctool
 def noctool(driver):
@@ -17,12 +20,13 @@ def noctool(driver):
  
             driver.get(n_webpage[i])
             wait(driver, '/html/body/div/div/main/div/div[3]/div[2]/div/div[1]/h5', '記錄量趨勢圖')       
-            time.sleep(1)
+            time.sleep(0.5)
             if i == 0:       
                 pyautogui.click(x=879, y=346)
                 time.sleep(1)
             pyautogui.scroll(-30)
-            time.sleep(1)
+
+            # Click
             pyautogui.click(x=180, y=668)
 
             # Previous Credit / Data
@@ -32,12 +36,14 @@ def noctool(driver):
             mangos_id = {'_id': ObjectId(mongodb_id[i])}
             documents = find_one(mangos_id)
             credit_value = documents.get('Credit', 'N/A') 
+            pyperclip.copy(credit_value)
 
             # Print out Previous and Actual Data
             print(f"{ID[i]}= Pre: {pre_credit}, Act: {credit_value} \n")
-            
+
             # Write Credit Value
-            pyautogui.write(credit_value)
+            # pyautogui.write(credit_value)
+            pyautogui.hotkey("Command", "V")
 
             # button click (新增记录)
             pyautogui.hotkey("Enter")
